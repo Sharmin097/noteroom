270e62d56fa7ac2209bcdc0e12a0628a2f80ea5c
[Sharmin097] <[sharmin.cse1.bu@gmail.com]>
2025-04-20 12:04:52 +0600

    Performed Follow user api

--- backend/services/apis/friends.ts
+++ backend/services/apis/friends.ts
@@ -76,6 +76,27 @@ export default function friendsApiRouter(io: Server) {
       data: friendRequests
     });
   });
+//follow a user
+
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followings: req.params.id } });
+        res.status(200).json("user has been followed");
+      } else {
+        res.status(403).json("you allready follow this user");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("you cant follow yourself");
+  }
+});
 
     
     return router
